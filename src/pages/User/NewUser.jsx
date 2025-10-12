164e744426353d2cda0bf71db5225e82255d4eea
Hugo Santos <139011452+hugh514@users.noreply.github.com>
2025-10-12 12:29:22 -0400

    feat: implementado o voltar usando navigate

--- src/pages/User/NewUser.jsx
+++ src/pages/User/NewUser.jsx
@@ -1,9 +1,14 @@
 import {useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 const NewUser = () => {
+  const navigate = useNavigate();
   const [inputNome, setNome] = useState();
   const [inputSobreNome, setSobreNome] = useState();
   const [inputEmail, setEmail] = useState();
+
+
+  const voltarPagina = () => navigate(-1);
   return (
     <div className="content">
       <h1>Criar Novo Usuário</h1>
@@ -26,7 +31,10 @@ const NewUser = () => {
           value={inputEmail}
           onChange={(e) => setEmail(e.target.value)}
         />
+        <div className="buttons">
         <button type="button" className="button create">Cadastrar</button>
+        <button type="button" className="button back" onClick={voltarPagina}>Voltar</button>
+        </div>
       </form>
     </div>
   );
